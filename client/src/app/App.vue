8ec721b5eae553e6d77c4dc6dcfdd078ded84490
AleksandrMelexin <Melehin-Sasha7@yandex.ru>
2025-11-03 17:37:08 +0300

    Создан проект

---
+++ client/src/app/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Hello world
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ client/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './app/App.vue'
+import vuetify from './app/vuetify'
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
